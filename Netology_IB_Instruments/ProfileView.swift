1c7961dba12f8778a590f50e43346592c083391c
Sinsl <sinsl@me.com>
2022-03-24 18:43:43 +0300

    Create ProfileView and ProfileViewController

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Mac Home on 24.03.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var image_user: UIImageView!
+    
+    @IBOutlet weak var name_user: UILabel!
+    
+    @IBOutlet weak var birthday_user: UILabel!
+    
+    @IBOutlet weak var city_user: UILabel!
+    
+    @IBOutlet weak var signature_user: UITextView!
+    
+}

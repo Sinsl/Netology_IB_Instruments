dfc2be0c0d571e4857bbdf65056d29d7ecbdac76
Sinsl <sinsl@me.com>
2022-03-24 18:54:24 +0300

    Создание profile.storiboard с контроллером. Подключение сториборда как основного

---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Mac Home on 24.03.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}

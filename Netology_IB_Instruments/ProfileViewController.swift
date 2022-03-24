d63d12fc3b47fab4612e9610185a74ed575d6e6f
Sinsl <sinsl@me.com>
2022-03-24 19:03:41 +0300

    Подключение ProfileView на созданный сториборд.

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -12,7 +12,12 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let screenWidth = UIScreen.main.bounds.width
+
+        if let profileView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            profileView.frame = CGRect(x: 16, y: 50, width: screenWidth - 32, height: 500)
+            view.addSubview(profileView)
+        }
     }
     
 }
